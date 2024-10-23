efc9a025fc239977a9839939baeb060f998ed39d
ReecerTV <r3769.tv@gmail.com>
2024-10-23 23:55:25 +0200

    maintainers: add reecertv

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -21587,6 +21587,12 @@
     githubId = 1391883;
     name = "Tom Hall";
   };
+  thtrf = {
+    email = "thtrf@proton.me";
+    github = "thtrf";
+    githubId = 82712122;
+    name = "thtrf";
+  };
   Thunderbottom = {
     email = "chinmaydpai@gmail.com";
     github = "Thunderbottom";
